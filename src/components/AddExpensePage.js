f19129ecfc0ebb6957c32229977d61d733d33685
Jason McDermott <jmcdermott@rightpoint.com>
2020-05-11 07:26:29 -0400

    inital project setup

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddExpensePage = () => (
+    <div>
+        Add Expense Component
+    </div>
+)
+
+export default AddExpensePage
---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExpenseDashboardPage = () => (
+    <div>
+        Expense Dashboard Component
+    </div>
+)
+
+export default ExpenseDashboardPage
